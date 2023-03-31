6e0a40dffc896f737533f7d1d89b6d7625156c95
Lemorshane <124408950+Lemorshane@users.noreply.github.com>
2023-03-31 22:16:42 +0300

    Update ConsoleApplication15.cpp

--- ConsoleApplication15/ConsoleApplication15.cpp
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -1,12 +1,35 @@
-﻿#include <ctime>
 #include <iostream>
 using std::cout;
 using std::cin;
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	srand(time(NULL));
-	int n;
-	n = rand() { 100 + 1 };
-	cout << n;
+	char op;
+	double num1;
+	double num2;
+	double result;
+	cout << "-------КАЛЬКУЛЯТОР-------\n";
+	cout << "Введите числа и функцию ( + - * /) по примеру:\n 123 * 123 :\n";
+	cin >> num1 >> op >> num2;
+	switch (op) {
+	case '+':
+		result = num1 + num2;
+		cout << " = " << result << "\n";
+		break;
+	case '-':
+		result = num1 - num2;
+		cout << " = " << result << "\n";
+		break;
+	case '*':
+		result = num1 * num2;
+		cout << " = " << result << "\n";
+		break;
+	case '/':
+		result = num1 / num2;
+		cout << " = " << result << "\n";
+		break;
+	}
+	cout << "-------------------------";
+
 }
+
