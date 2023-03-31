0495950ffde8fc6039ea81f7db015e1e25ba1a5a
INTEL <INTEL@DESKTOP-BSQ2HKP>
2023-03-31 22:10:36 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication15/ConsoleApplication15.cpp
@@ -0,0 +1,12 @@
+﻿#include <ctime>
+#include <iostream>
+using std::cout;
+using std::cin;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	srand(time(NULL));
+	int n;
+	n = rand() { 100 + 1 };
+	cout << n;
+}
